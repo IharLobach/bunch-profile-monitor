4192d993cf495b6c1645fb472342ecdf0c12a9bd
Ihar Lobach <lobachihor1994@gmail.com>
2020-02-03 15:29:58 -0600

    added module bunch_profile_data_updater.py and started bokeh_gui.py

---
+++ bunch_profile_monitor_data_updater.py
@@ -0,0 +1,26 @@
+import threading
+class bpm_data_updater(threading.Thread):
+    daemon = True
+
+    def __init__(self, bpm,use_test_data,signal_transfer_line,new_data_to_show_queue,new_data_to_save_queue):
+        super().__init__()
+        self.bpm = bpm
+        self.use_test_data = use_test_data
+        self.signal_transfer_line = signal_transfer_line
+        self.new_data_to_show_queue = new_data_to_show_queue
+        self.new_data_to_save_queue = new_data_to_save_queue
+
+    def run(self):
+        while True:
+            update_successful = self.bpm.update_data(testing=self.use_test_data)
+            self.bpm.perform_fft()
+            self.bpm.perform_signal_reconstruction()
+            self.bpm.calc_fwhm()
+            if update_successful:
+                with self.new_data_to_show_queue.mutex:
+                    self.new_data_to_show_queue.queue.clear()
+                self.new_data_to_show_queue.put({"reconstructed_signal":self.bpm.reconstructed_signal,"fwhm":self.bpm.fwhm})
+                with self.new_data_to_save_queue.mutex:
+                    self.new_data_to_save_queue.queue.clear()
+                self.new_data_to_save_queue.put({"oscilloscope_signal":self.bpm.v_arr,
+                "reconstructed_signal":self.bpm.reconstructed_signal,"fwhm":self.bpm.fwhm})
