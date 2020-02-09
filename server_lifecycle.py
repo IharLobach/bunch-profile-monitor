8b520654e20b1cf9b4e2810bb6192e990702870e
Ihar Lobach <lobachihor1994@gmail.com>
2020-02-09 16:57:33 -0600

    added redirecting the url to port 5000 with flask

--- server_lifecycle.py
+++ server_lifecycle.py
@@ -6,3 +6,4 @@ def on_server_loaded(server_context):
     if not os.path.exists(os.path.join(os.getcwd(),"bunch-profile-monitor","log.db")):
         exec(open(os.path.join(os.getcwd(),"bunch-profile-monitor","create_db.py")).read())
     webbrowser.open_new(get_from_config("url"))
+    os.system("python3 ~/bunch-profile-monitor/redirect_url.py &")
