233cc38e141d7ff00151d442c72fb791f4b1c41f
Ihar Lobach <lobachihor1994@gmail.com>
2020-02-09 17:03:44 -0600

    added url redirect with flask to port 5000

---
+++ redirect_url.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask,redirect
+from config_requests import get_from_config
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return redirect(get_from_config("url"), code=302)
+
+if __name__ == '__main__':
+    #default port is 5000
+    app.run(host='0.0.0.0')
